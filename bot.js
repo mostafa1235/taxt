2ec69920f0c12fb16c066b9aaaa367a0e3c3f4f3
mostafa1235 <48168138+mostafa1235@users.noreply.github.com>
2019-03-03 06:45:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,18 @@ client.on('message', message => {
       }
 });
 
+client.on('message', message => {
+    if (message.content === 'باك') {
+        message.reply('ولكم ');
+      }
+});
+
+
+client.on('message', message => {
+    if (message.content === 'برب') {
+        message.reply('في دهيه ');
+      }
+});
 
 
 
