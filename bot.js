3a7fa2475dda5ad0a0381dae3c665ab055b16300
mostafa1235 <48168138+mostafa1235@users.noreply.github.com>
2019-03-03 06:17:08 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "-"
+  
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`pixel Bot- Script By : Hamo-zine-lizer`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Zine-Hamo-lizer ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`-help | ${client.guilds.size} Servers `,"http://twitch.tv/Pixel Bot")
+client.user.setStatus("dnd")
+});
+
+
+client.login(process.env.BOT_TOKEN); 
